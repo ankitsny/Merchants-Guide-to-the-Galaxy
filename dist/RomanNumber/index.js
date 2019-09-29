c0393034eb5db100f99a3dda4048f6ed78b2664a
Ankit kumar <ankit_sony3@hotmail.com>
2019-09-29 18:55:26 +0530

    ignore dist file

--- dist/RomanNumber/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RomanNumberBuilder_1 = require("./RomanNumberBuilder");
-(function T() {
-    var x = new RomanNumberBuilder_1.RomanNumberBuilder();
-    var rs = x.build();
-    rs.setAlias("I", "ANKSO");
-    console.log(rs.getSymbolFromAlias("ANKSO"));
-})();
